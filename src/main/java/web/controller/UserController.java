4ebd01cfb597f2e084a19bceca4a78370f109da9
zirpav <zir-pav@mail.ru>
2020-09-03 21:34:15 +0300

    В первом приближении все работает.

---
+++ src/main/java/web/controller/UserController.java
@@ -0,0 +1,32 @@
+package web.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+import web.model.User;
+import web.service.UserService;
+
+@Controller
+public class UserController {
+
+    private UserService userService;
+
+    @Autowired
+    public void setUserService(UserService userService) {
+        this.userService = userService;
+    }
+
+
+    @GetMapping("/user")
+    public String userInfo(Model model) {
+        User user = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        model.addAttribute("userData", user);
+        return "user";
+    }
+
+
+}
